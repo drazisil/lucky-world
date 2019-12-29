d2ae699bdb773741efeddc5e525aa208818f28bb
Joe Becher <jwbecher@drazisil.com>
2019-12-29 11:45:27 -0500

    boom

--- src/main/java/com/drazisil/luckyworld/LWListener.java
+++ src/main/java/com/drazisil/luckyworld/LWListener.java
@@ -2,19 +2,24 @@ package com.drazisil.luckyworld;
 
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import static com.drazisil.luckyworld.LuckyWorld.getMagicNumber;
-import static com.drazisil.luckyworld.LuckyWorld.getMaxNumber;
-import static com.drazisil.luckyworld.LuckyWorld.worldHandler;
+import java.util.List;
+
+import static com.drazisil.luckyworld.LuckyWorld.*;
 import static com.drazisil.luckyworld.event.LWEventHandler.handleLuckyEvent;
 import static com.drazisil.luckyworld.event.LWEventHandler.shouldEvent;
 import static com.drazisil.luckyworld.shared.LWUtilities.cleanLocation;
+import static org.bukkit.entity.EntityType.PRIMED_TNT;
 
 
 class LWListener implements Listener {
@@ -24,6 +29,31 @@ class LWListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getName() + " to..." + LuckyWorld.name + "!");
+
+    }
+
+    @EventHandler
+    public void onEntityExplodePrime(ExplosionPrimeEvent event) {
+        System.out.println(event.getEntityType() + " = " + event.getRadius());
+        if (event.getEntityType().equals(PRIMED_TNT)) {
+            event.setRadius(event.getRadius() * 20);
+        }
+
+    }
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        System.out.println(event.getEntityType());
+        if (event.getEntityType().equals(PRIMED_TNT)) {
+            List<Block> blocks = event.blockList();
+            for (Block block: blocks
+                    ) {
+                block.getWorld().getBlockAt(block.getLocation()).setType(Material.GLOWSTONE);
+
+            }
+            event.setCancelled(true);
+        }
+
 
     }
 
