0efce4eb32b1dfc2b2fddaa13a2abcd76aa1673a
Joe Becher <jwbecher@drazisil.com>
2020-02-07 23:59:11 -0500

    desk la pig

--- src/main/java/com/drazisil/luckyworld/event/EventClassroom.java
+++ src/main/java/com/drazisil/luckyworld/event/EventClassroom.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.type.TrapDoor;
 import org.bukkit.entity.Pig;
 import org.bukkit.entity.Player;
@@ -22,7 +23,72 @@ public class EventClassroom extends LuckyEvent {
     @Override
     public void doAction(BlockBreakEvent event, World world, Location location, Player player) {
 
-        RoundLocation deskSeatSpawnLoc = (RoundLocation) LWUtilities.cleanLocation(location.clone()).add(-4, -1, 0);
+        RoundLocation deskSeatSpawnLoc = (RoundLocation) LWUtilities.cleanLocation(location.clone()).add(0, -1, -3);
+        Pig playerSeat = makeDeskChair(world, deskSeatSpawnLoc);
+
+        RoundLocation deskSpawnLoc = (RoundLocation) LWUtilities.cleanLocation(location.clone()).add(0, 0, -4);
+        makeDesk(world, deskSpawnLoc);
+
+
+        Location seatLocation = playerSeat.getLocation().clone();
+        player.teleport(new Location(world, seatLocation.getX(), seatLocation.getY(), seatLocation.getZ(), 180.0f, 0.0f));
+        playerSeat.addPassenger(player);
+
+
+//        RoundLocation leftDeskSeatLoc = (RoundLocation) deskSeatSpawnLoc.clone().add(-1.0, 1.0, -0.0);
+//        Block trapdoorLeftSeatBlock = world.getBlockAt(leftDeskSeatLoc);
+//        trapdoorLeftSeatBlock.setType(Material.ACACIA_TRAPDOOR);
+//        TrapDoor trapdoorLeftSeatData = (TrapDoor) trapdoorLeftSeatBlock.getBlockData();
+//        trapdoorLeftSeatData.setFacing(BlockFace.EAST);
+//        trapdoorLeftSeatData.setOpen(true);
+//        trapdoorLeftSeatBlock.setBlockData(trapdoorLeftSeatData);
+//
+//        RoundLocation rightDeskSeatLoc = (RoundLocation) deskSeatSpawnLoc.clone().add(1.0, 1.0, -0.0);
+//        Block trapdoorRightSeatBlock = world.getBlockAt(rightDeskSeatLoc);
+//        trapdoorLeftSeatBlock.setType(Material.ACACIA_TRAPDOOR);
+//        TrapDoor trapdoorRightSeatData = (TrapDoor) trapdoorLeftSeatBlock.getBlockData();
+//        trapdoorRightSeatData.setFacing(BlockFace.EAST);
+//        trapdoorRightSeatData.setOpen(true);
+//        trapdoorRightSeatBlock.setBlockData(trapdoorRightSeatData);
+
+
+    }
+
+    private void placeSign(RoundLocation location, Material material, BlockFace face) {
+        RoundLocation rightDeskSeatLoc = location;
+        Block trapdoorRightSeatBlock = location.getWorld().getBlockAt(rightDeskSeatLoc);
+        trapdoorRightSeatBlock.setType(material);
+        org.bukkit.block.data.type.WallSign trapdoorRightSeatData = ((org.bukkit.block.data.type.WallSign) trapdoorRightSeatBlock.getBlockData());
+        trapdoorRightSeatData.setFacing(face);
+        trapdoorRightSeatBlock.setBlockData(trapdoorRightSeatData);
+    }
+
+    private void makeDesk(World world, Location location) {
+        Block trapdoorDeskBlock = world.getBlockAt(location.clone());
+        trapdoorDeskBlock.setType(Material.BIRCH_TRAPDOOR);
+        TrapDoor trapdoorDeskData = (TrapDoor) trapdoorDeskBlock.getBlockData();
+        trapdoorDeskData.setFacing(BlockFace.SOUTH);
+        trapdoorDeskData.setHalf(Bisected.Half.TOP);
+        trapdoorDeskBlock.setBlockData(trapdoorDeskData);
+
+        Block trapdoorDeskLeft = world.getBlockAt(location.clone().add(-1.0, 0.0, 0.0));
+        trapdoorDeskLeft.setType(Material.ACACIA_TRAPDOOR);
+        TrapDoor trapdoorDeskLeftData = (TrapDoor) trapdoorDeskLeft.getBlockData();
+        trapdoorDeskLeftData.setFacing(BlockFace.WEST);
+        trapdoorDeskLeftData.setOpen(true);
+        trapdoorDeskLeft.setBlockData(trapdoorDeskLeftData);
+
+        Block trapdoorDeskRight = world.getBlockAt(location.clone().add(1.0, 0.0, 0.0));
+        trapdoorDeskRight.setType(Material.ACACIA_TRAPDOOR);
+        TrapDoor trapdoorDeskRightData = (TrapDoor) trapdoorDeskRight.getBlockData();
+        trapdoorDeskRightData.setFacing(BlockFace.EAST);
+        trapdoorDeskRightData.setOpen(true);
+        trapdoorDeskRight.setBlockData(trapdoorDeskRightData);
+
+    }
+
+    private Pig makeDeskChair(World world, Location location) {
+        Location deskSeatSpawnLoc = location;
 
         world.getBlockAt(deskSeatSpawnLoc).setType(Material.ACACIA_SLAB);
         RideableMinecart rideableMinecart = world.spawn(deskSeatSpawnLoc.clone().add(0.5, 0.5, 0.5), RideableMinecart.class);
@@ -31,7 +97,10 @@ public class EventClassroom extends LuckyEvent {
         Pig pigSeat = world.spawn(deskSeatSpawnLoc, Pig.class);
         pigSeat.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, effectDuration, 1));
         pigSeat.setSilent(true);
+        pigSeat.setAI(false);
         pigSeat.setSaddle(true);
+        pigSeat.setInvulnerable(true);
+        pigSeat.setRotation(180, 0);
         pigSeat.setCustomName("classroom_seat");
         pigSeat.setCustomNameVisible(false);
 
@@ -54,32 +123,7 @@ public class EventClassroom extends LuckyEvent {
         trapdoorBackSeatData.setOpen(true);
         trapdoorBackSeatBlock.setBlockData(trapdoorBackSeatData);
 
-//        RoundLocation leftDeskSeatLoc = (RoundLocation) deskSeatSpawnLoc.clone().add(-1.0, 1.0, -0.0);
-//        Block trapdoorLeftSeatBlock = world.getBlockAt(leftDeskSeatLoc);
-//        trapdoorLeftSeatBlock.setType(Material.ACACIA_TRAPDOOR);
-//        TrapDoor trapdoorLeftSeatData = (TrapDoor) trapdoorLeftSeatBlock.getBlockData();
-//        trapdoorLeftSeatData.setFacing(BlockFace.EAST);
-//        trapdoorLeftSeatData.setOpen(true);
-//        trapdoorLeftSeatBlock.setBlockData(trapdoorLeftSeatData);
-//
-//        RoundLocation rightDeskSeatLoc = (RoundLocation) deskSeatSpawnLoc.clone().add(1.0, 1.0, -0.0);
-//        Block trapdoorRightSeatBlock = world.getBlockAt(rightDeskSeatLoc);
-//        trapdoorLeftSeatBlock.setType(Material.ACACIA_TRAPDOOR);
-//        TrapDoor trapdoorRightSeatData = (TrapDoor) trapdoorLeftSeatBlock.getBlockData();
-//        trapdoorRightSeatData.setFacing(BlockFace.EAST);
-//        trapdoorRightSeatData.setOpen(true);
-//        trapdoorRightSeatBlock.setBlockData(trapdoorRightSeatData);
-
-        pigSeat.addPassenger(player);
-    }
-
-    private void placeSign(RoundLocation location, Material material, BlockFace face) {
-        RoundLocation rightDeskSeatLoc = location;
-        Block trapdoorRightSeatBlock = location.getWorld().getBlockAt(rightDeskSeatLoc);
-        trapdoorRightSeatBlock.setType(material);
-        org.bukkit.block.data.type.WallSign trapdoorRightSeatData = ((org.bukkit.block.data.type.WallSign) trapdoorRightSeatBlock.getBlockData());
-        trapdoorRightSeatData.setFacing(face);
-        trapdoorRightSeatBlock.setBlockData(trapdoorRightSeatData);
+        return pigSeat;
     }
 
 
